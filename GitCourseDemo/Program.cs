e2c343120ee71a43a1b9aedf140fb0391be9fd81
Mashmori <mohammed.alashmori@yemenmobile.com.ye>
2026-09-15 16:28:28 +0300

    decrease welcome messages

--- GitCourseDemo/Program.cs
+++ GitCourseDemo/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-var x = 0;
+var x = 5;
 
-while (x < 5)
+while (x > 5)
 {
     Console.WriteLine($"welcome to GitCourse {x}");
-    x++;
+    x--;
 }
